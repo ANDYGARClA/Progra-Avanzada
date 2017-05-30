6638e78f4ce83697006d177ada7cd77c606e33d0
Andygarcla <andy12_garsa@hotmail.com>
2017-05-29 20:28:22 -0600

    Se agregan cambios.

---
+++ Ulatina.PrograAvanzada.Calentamiento/Ulatina.PrograAvanzada.BLAplicacionWeb/Dominio/Especificaciones/ObtengaFecha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada.BLAplicacionWeb.Dominio.Especificaciones
+{
+    public class ObtengaFecha
+    {
+        public string FormatearUnaFecha(DateTime laFecha, string elFormato) {
+
+            return "Hola mundo";
+
+        }
+
+    }
+}
